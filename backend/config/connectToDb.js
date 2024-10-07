46456ba76e9a8c60564125a1ba764020efb7f010
mmarque <marquezmusa@gmail.com>
2024-10-07 10:02:03 -0400

    update markdown file readme.md

--- backend/config/connectToDb.js
+++ backend/config/connectToDb.js
@@ -2,6 +2,6 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 const connectToDb = async () => {
   mongoose.connect(process.env.DB_URL);
-  console.log("backend/config/connectToDB ▶️ ▶️ ▶️ Connected to DB");
+  console.log("backend/config/connectToDB  ▶️ ▶️ ▶️ Connected to DB");
 };
 module.exports = connectToDb;
--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,14 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 //-->-->-->-->-->--> Mongoose
+console.log("< package script: npm start >");
+
 const connectToDb = require("./config/connectToDb");
 connectToDb();
 
 //-->-->-->-->-->--> Server
 app.listen(PORT, () => {
-  console.log("< package script: npm start >");
-  console.log(`server.js ▶️ ▶️ ▶️ Listening on port ${PORT}`);
+  console.log(
+    `server.js ▶️ ▶️ ▶️ ▶️ ▶️ ▶️ ▶️ ▶️ ▶️ ▶️ ▶️ ▶️ Listening on port ${PORT}`
+  );
 });
